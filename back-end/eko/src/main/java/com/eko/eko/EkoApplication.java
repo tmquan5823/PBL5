6a9833e5dad570a190ed42e313d1aab5fb27118c
Võ Viết Trường <voviettruong141003@gmail.com>
2024-04-24 20:58:30 +0700

    CreateServer

---
+++ back-end/eko/src/main/java/com/eko/eko/EkoApplication.java
@@ -0,0 +1,13 @@
+package com.eko.eko;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EkoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EkoApplication.class, args);
+	}
+
+}
